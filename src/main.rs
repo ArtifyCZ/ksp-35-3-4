9c0cab2b8bcbfe044447458edb14a3850def8b46
Richard Tichý <tichy@artify.zone>
2023-02-18 21:16:55 +0100

    Implemented input reading

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+use std::io::stdin;
+use anyhow::Result;
+use token_read::TokenReader;
+
+#[derive(Debug)]
+struct TeaBox {
+    index: usize,
+    weight: usize,
+    has_cover: bool,
+}
+
+#[derive(Debug, Default)]
+struct Boxes {
+    boxes: Vec<TeaBox>,
+}
+
+impl Boxes {
+    pub fn get(&self, index: usize) -> Option<&TeaBox> {
+        self.boxes.get(index - 1)
+    }
+
+    pub fn push(&mut self, mut tea: TeaBox) -> usize {
+        let index = self.boxes.len() + 1;
+        tea.index = index;
+        self.boxes.push(tea);
+        index
+    }
+}
+
+#[derive(Debug)]
+struct Input {
+    boxes_count: usize, // N
+    string_length: usize, // K
+    boxes: Boxes,
+}
+
+fn read_input() -> Result<Input> {
+    let mut input = TokenReader::new(stdin().lock());
+
+    let (n, k): (usize, usize) = input.line()?;
+
+    let mut boxes = Boxes::default();
+
+    for line in input.take(n) {
+        let (m, v): (usize, u8) = line?;
+        let v = v == 1;
+
+        boxes.push(TeaBox {
+            index: 0, // index is automatically added
+            weight: m,
+            has_cover: v,
+        });
+    }
+
+    Ok(Input {
+        boxes_count: n,
+        string_length: k,
+        boxes,
+    })
+}
+
+fn main() -> Result<()> {
+    let input = read_input()?;
+
+    dbg!(input);
+
+    Ok(())
+}
